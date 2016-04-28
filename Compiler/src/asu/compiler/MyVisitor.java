e55dfe41d9a9aaad1a096a5e5182152fcc43991c
adityab17190 <abivalka@asu.edu>
2016-04-28 05:23:52 -0700

    Implemented recursion and stack

--- Compiler/src/asu/compiler/MyVisitor.java
+++ Compiler/src/asu/compiler/MyVisitor.java
@@ -6,6 +6,7 @@ import java.util.Stack;
 
 import asu.parser.DemoBaseVisitor;
 import asu.parser.DemoParser.AssignContext;
+import asu.parser.DemoParser.AssignwithfunccallContext;
 import asu.parser.DemoParser.BexpContext;
 import asu.parser.DemoParser.BlockContext;
 import asu.parser.DemoParser.ComparisonContext;
@@ -14,16 +15,29 @@ import asu.parser.DemoParser.ConditionalContext;
 import asu.parser.DemoParser.DeclareContext;
 import asu.parser.DemoParser.ElseblockContext;
 import asu.parser.DemoParser.EqualityContext;
+import asu.parser.DemoParser.FuncCallContext;
+import asu.parser.DemoParser.FuncDeclareContext;
+import asu.parser.DemoParser.FuncargsContext;
+import asu.parser.DemoParser.FuncblockContext;
+import asu.parser.DemoParser.FunccallargsContext;
 import asu.parser.DemoParser.IexpContext;
+import asu.parser.DemoParser.IfblockContext;
 import asu.parser.DemoParser.MainContext;
+import asu.parser.DemoParser.PopContext;
+import asu.parser.DemoParser.PrintContext;
+import asu.parser.DemoParser.PrintlnContext;
 import asu.parser.DemoParser.ProgramContext;
+import asu.parser.DemoParser.PushContext;
+import asu.parser.DemoParser.ReturnexpContext;
+import asu.parser.DemoParser.StackdeclareContext;
 
 public class MyVisitor extends DemoBaseVisitor<String> {
 
     private static int count = -1;
     public static List<String> statements = new ArrayList<>();
-    private static Stack<Integer> indexStack = new Stack<>();
-
+    private static Stack<Integer> conditionStack = new Stack<>();
+    private static Stack<Integer> whileIndexStack = new Stack<>();
+     
     /**
      * @param ctx
      * @return returns final program string (op code)
@@ -66,7 +80,16 @@ public class MyVisitor extends DemoBaseVisitor<String> {
             count += 1;
             statements.add(getIOperation(ctx.getChild(1).getText()) + " $" + count + "," + temp.replace("\n", ",") + "," + ctx.INT().getText());
             return "$" + count;
-        } else {
+        } 
+        
+        else if(ctx.IDENTIFIER()!=null){
+        	count += 1;
+            statements.add(getIOperation(ctx.getChild(1).getText()) + " $" + count + "," + temp.replace("\n", ",") + "," + ctx.IDENTIFIER().getText());
+            return "$" + count;
+        	
+        }
+        
+        else {
             count += 1;
             statements.add(getIOperation(ctx.getChild(1).getText()) + " $" + count + "," + temp.replace("\n", ","));
             return "$" + count;
@@ -74,7 +97,7 @@ public class MyVisitor extends DemoBaseVisitor<String> {
     }
 
     /**
-     * @param ctx
+     *  @param ctx
      * @return returns the op code for assignment statements
      */
     @Override
@@ -84,6 +107,7 @@ public class MyVisitor extends DemoBaseVisitor<String> {
         return null;
     }
 
+    
     /**
      * @param ctx
      * @return returns the op code for boolean expressions
@@ -96,7 +120,10 @@ public class MyVisitor extends DemoBaseVisitor<String> {
             statements.add(ctx.getChild(0).getText() + " $" + count + " " + ctx.getChild(2).getText());
             return "$" + count;
         } else if(ctx.BOOL() != null) {
-            return ctx.BOOL().getText();
+            count += 1;
+            statements.add("STORE " + " $" + count + " " + ctx.getText());
+            return "$" + count;
+
         }
         return ctx.getChild(0).getText();
     }
@@ -158,10 +185,10 @@ public class MyVisitor extends DemoBaseVisitor<String> {
     @Override
     public String visitBlock(BlockContext ctx) {
         if(ctx.getChildCount() > 0) {
-            if(ctx.getChild(0).getText().equals("IF")) {
+           /* if(ctx.getChild(0).getText().equals("IF")) {
                 statements.add("IF");
                 visitChildren(ctx);
-                int index=indexStack.pop();
+                int index=ifIndexStack.pop();
                 String stmt = statements.get(index);
                 stmt += "," + (statements.size() + 1);
                 if(ctx.getChildCount()==3)
@@ -169,9 +196,25 @@ public class MyVisitor extends DemoBaseVisitor<String> {
                 statements.set(index, stmt);
                 
                
-            } else if(ctx.getChild(0).getText().equals("{")) {
+            } else*/
+        	if(ctx.getChild(0).getText().equals("WHILE")){
+        		statements.add("LOOP");
+        		whileIndexStack.push(statements.size());
+        		visitChildren(ctx);
+        		int index=conditionStack.pop();
+        		String stmt = statements.get(index);
+                stmt += "," + (statements.size() + 1);
+                statements.set(index, stmt);
+                whileIndexStack.pop();
+        	}
+        	
+        	else if(ctx.getChild(0).getText().equals("{")) {
                 statements.add("BSTART");
                 visitChildren(ctx);
+                if(!whileIndexStack.isEmpty()){
+                	int index=whileIndexStack.peek();
+                	statements.add("GOTO "+index);
+                }
                 statements.add("BEND");
             } else {
                 visitChildren(ctx);
@@ -181,9 +224,25 @@ public class MyVisitor extends DemoBaseVisitor<String> {
     }
 
     @Override
+    public String visitIfblock(IfblockContext ctx) {
+    	// TODO Auto-generated method stub
+    	//return super.visitIfblock(ctx);
+    	statements.add("IF");
+    	visitChildren(ctx);
+    	int index=conditionStack.pop();
+        String stmt = statements.get(index);
+        stmt += "," + (statements.size() + 1);
+        if(ctx.getChildCount()==3)
+            stmt += "," + (statements.size() + 1);
+        statements.set(index, stmt);
+        return "";
+
+    }
+    
+    @Override
     public String visitElseblock(ElseblockContext ctx) {
         statements.add("ELSE");
-        int index=indexStack.peek();
+        int index=conditionStack.peek();
         String stmt = statements.get(index);
         stmt += "," + (statements.size());
         statements.set(index, stmt);
@@ -191,12 +250,13 @@ public class MyVisitor extends DemoBaseVisitor<String> {
         return "";
     }
 
+    
     @Override
     public String visitCondition(ConditionContext ctx) {
         visitChildren(ctx);
         statements.add("CHECKT $" + count);
         int index = statements.size() - 1;
-        indexStack.push(index);
+        conditionStack.push(index);
         return "";
     }
 
@@ -208,11 +268,111 @@ public class MyVisitor extends DemoBaseVisitor<String> {
         return "";
     }
 
+   
+    @Override
+    public String visitFuncblock(FuncblockContext ctx) {
+    	// TODO Auto-generated method stub
+          	
+    	statements.add("BSTART");
+    	visitChildren(ctx);
+    	statements.add("BEND");
+    	return "";
+    }
+    
+    @Override
+    public String visitFuncDeclare(FuncDeclareContext ctx) {
+    	// TODO Auto-generated method stub
+    	System.out.println(ctx.getText());
+    	statements.add("FUNC "+ctx.getChild(2).getText()+",");
+    	visitChildren(ctx);
+    	return "";
+    }
+    
+    
+    @Override
+    public String visitFuncargs(FuncargsContext ctx) {
+    	// TODO Auto-generated method stub
+    	//return super.visitFuncargs(ctx);
+    	int index=statements.size()-1;
+    	String statement=statements.get(index);
+    	int len=ctx.getChildCount();
+        if(len>2){
+    	statement+=ctx.getChild(1).getText();
+        for(int i=3;i<len;i+=2){
+        	statement+=","+ctx.getChild(i);
+        }
+        }
+        statements.set(index, statement);
+        return "";
+    }
+    
+    @Override
+    public String visitReturnexp(ReturnexpContext ctx) {
+    	// TODO Auto-generated method stub
+    	String temp=visitChildren(ctx);
+    	temp=temp.replace("\n", "");
+    	String statement="RETURN "+temp;
+    	statements.add(statement);
+    	return "";
+    	//return super.visitReturnexp(ctx);
+    }
+   
+ 
+    @Override
+    public String visitFunccallargs(FunccallargsContext ctx) {
+    	// TODO Auto-generated method stub
+    	int index=statements.size()-1;
+    	String statement=statements.get(index);
+    	int len=ctx.getChildCount();
+        if(len>2){
+    	statement+=ctx.getChild(1).getText();
+        for(int i=3;i<len;i+=2){
+        	statement+=","+ctx.getChild(i);
+        }
+        }
+        statements.set(index, statement);
+        return "";
+    
+    }  
+
+    @Override
+    public String visitFuncCall(FuncCallContext ctx) {
+    	// TODO Auto-generated method stub
+    	statements.add("GOTO "+ctx.getChild(0).getText()+",");
+    	visitChildren(ctx);
+    	return "";
+     
+    }
+    
+    @Override
+    public String visitAssignwithfunccall(AssignwithfunccallContext ctx) {
+    	// TODO Auto-generated method stub
+      visitChildren(ctx);
+      statements.add("STORE "+ctx.getChild(0).getText()+",POP()");
+      return "";
+    }
+    
+     
     /**
      * @param aggregate
      * @param nextResult
      * @return concatenates result strings
      */
+    
+    @Override
+    public String visitPrint(PrintContext ctx) {
+    	// TODO Auto-generated method stub
+    statements.add("PRINT "+ctx.getChild(2));
+    return "";
+    }
+    
+    @Override
+    public String visitPrintln(PrintlnContext ctx) {
+    	// TODO Auto-generated method stub
+    	statements.add("PRINTLN "+ctx.getChild(2));
+        return "";    
+    }
+    
     @Override
     protected String aggregateResult(String aggregate, String nextResult) {
         if(aggregate == null && nextResult == null) return "";
@@ -221,6 +381,38 @@ public class MyVisitor extends DemoBaseVisitor<String> {
         return aggregate + "\n" + nextResult;
     }
 
+    @Override
+    public String visitStackdeclare(StackdeclareContext ctx) {
+    	// TODO Auto-generated method stub
+        if(ctx.getChildCount()==3){
+        	if(ctx.getChild(0).getText().equals("STACKI"))
+        		statements.add("DECLARESI "+ctx.getChild(2).getText());
+        	else if(ctx.getChild(0).getText().equals("STACKB"))
+        		statements.add("DECLARESB "+ctx.getChild(2).getText());
+        	else
+        		statements.add("DECLARESS "+ctx.getChild(2).getText());
+        		
+        }
+    return "";	
+    }
+   
+    @Override
+    public String visitPush(PushContext ctx) {
+    	// TODO Auto-generated method stub
+    if(ctx.getChildCount()==6){	
+    statements.add("PUSH "+ctx.getChild(2).getText()+","+ctx.getChild(4).getText());
+    }
+    return "";
+    }
+   
+    @Override
+    public String visitPop(PopContext ctx) {
+    	// TODO Auto-generated method stub
+    	if(ctx.getChildCount()==6){	
+    	    statements.add("POP "+ctx.getChild(2).getText()+","+ctx.getChild(4).getText());
+    	    }
+    	    return "";
+    	  }
     /**
      * @param operation
      * @return return the right integer operation op code
@@ -229,6 +421,7 @@ public class MyVisitor extends DemoBaseVisitor<String> {
         if(operation.equals("+")) return "ADD";
         if(operation.equals("-")) return "SUB";
         if(operation.equals("*")) return "MUL";
+        if(operation.equals("%")) return "REM";
         return "DIV";
     }
 
--- Parser/Grammar/Demo.g4
+++ Parser/Grammar/Demo.g4
@@ -8,16 +8,22 @@ global:((declare NEWLINE)+(assign NEWLINE)*(funcDeclare)*)+
 	  
 main:MSTART block MEND NEWLINE
     |MSTART NEWLINE (exp NEWLINE)+ block (exp NEWLINE)* MEND NEWLINE
-	|MSTART NEWLINE (exp NEWLINE)* block (exp NEWLINE)+ MEND NEWLINE;
-	  
+	|MSTART NEWLINE (exp NEWLINE)* block (exp NEWLINE)+ MEND NEWLINE
+	|MSTART NEWLINE block MEND NEWLINE;  
+
+ifblock:IF condition block
+       |IF condition block elseblock;  	
+	
 elseblock:ELSE (WS)*block;	  
 	  
 block:(exp NEWLINE)*
-			|BSTART NEWLINE block BEND NEWLINE
-			|IF condition block
-            |IF condition block elseblock 
-	        |WHILE '('conditional')' block;			
+      |(exp NEWLINE)+ block 
+      |block (exp NEWLINE)+
+	  |BSTART NEWLINE block BEND NEWLINE
+	  |ifblock
+	  |WHILE condition block;			
              
+			 
 condition:'('conditional')';			 
 			 
 funcblock: BSTART NEWLINE block (returnexp NEWLINE)? BEND NEWLINE;
@@ -27,8 +33,6 @@ IF:'IF';
 WHILE:'WHILE';
  
 ELSE:'ELSE';
-
-ifblock:IF WS '(' conditional ')';
 	  	  
 NEWLINE:'\n';         	
 			
@@ -36,15 +40,34 @@ INT: [0-9]+;
 				 
 BOOL:'T'|'F' ;
 
+STRING:'\".*?\"';
+
 BSTART:'{';
 
 BEND:'}';
 
-exp:  iexp
+exp:  print
+     |println
+     |iexp
      |conditional
 	 |declare
 	 |assign
-	 |funcCall;
+	 |assignwithfunccall
+	 |funcCall
+	 |stackdeclare
+	 |push
+	 |pop;
+
+print: 'PRINT' WS STRING
+      |'PRINT' WS IDENTIFIER
+      |'PRINT' WS INT
+      |'PRINT' WS BOOL;	 
+      
+println: 'PRINTLN' WS STRING
+      |'PRINTLN' WS IDENTIFIER
+      |'PRINTLN' WS INT
+      |'PRINTLN' WS BOOL;	 
+            
 	 
 iexp: '(' iexp ')' 
       |iexp MULDIV INT  	 
@@ -55,7 +78,7 @@ iexp: '(' iexp ')'
 	  |IDENTIFIER;
 
 ADDSUB: ('+'|'-');  
-MULDIV: ('*'|'/');
+MULDIV: ('*'|'/'|'%');
 
 conditional:'(' conditional ')'
 		   |NOT WS conditional
@@ -110,16 +133,26 @@ COMPARE:('<'|'>'|'<='|'>=');
  
 DATATYPE:('INTEGER'|'BOOLEAN'|'VOID');
 
+STACKI:'STACKI';
+
+STACKB:'STACKB';
+
+STACKSTR:'STACKSTR';
+
 IDENTIFIER:[A-Za-z]+;
 
-declare:DATATYPE WS IDENTIFIER; 
-
+declare: DATATYPE WS IDENTIFIER;
+ 
+stackdeclare: STACKI WS IDENTIFIER
+             |STACKB WS IDENTIFIER 
+             |STACKSTR WS IDENTIFIER; 
+        
 WS:(' ')+;
 
-assign: IDENTIFIER ASSIGNMENT INT
-	   |IDENTIFIER ASSIGNMENT iexp
-	   |IDENTIFIER ASSIGNMENT conditional
-	   |IDENTIFIER ASSIGNMENT funcCall;
+assign:IDENTIFIER ASSIGNMENT iexp
+	   |IDENTIFIER ASSIGNMENT conditional;
+	   	   
+assignwithfunccall:IDENTIFIER ASSIGNMENT funcCall;
 	   
 ASSIGNMENT:'='; 
 
@@ -131,11 +164,21 @@ MSTART:'--';
 
 MEND:'!!';
 
-funcDeclare:'FUNCTION' WS IDENTIFIER '('')' funcblock
-           |'FUNCTION' WS IDENTIFIER '(' IDENTIFIER (','IDENTIFIER)* ')' funcblock;
-		   
+funcargs:'('')'
+         |'(' IDENTIFIER (','IDENTIFIER)* ')';
+
+funccallargs:'('')'
+            |'('(INT|BOOL|IDENTIFIER|iexp|conditional)(','(INT|BOOL|IDENTIFIER|iexp|conditional))*')';
+
+funcDeclare:'FUNCTION' WS IDENTIFIER funcargs funcblock;
 returnexp:  'RETURN' WS iexp
           | 'RETURN' WS conditional;  
 
-funcCall:IDENTIFIER'('')'
-        |IDENTIFIER'('(INT|BOOL|IDENTIFIER)(','(INT|BOOL|IDENTIFIER))*')';  		  
+funcCall:IDENTIFIER funccallargs;
+
+push:'PUSH''('IDENTIFIER','INT')'
+     |'PUSH''('IDENTIFIER','BOOL')'
+     |'PUSH''('IDENTIFIER','STRING')'
+     |'PUSH''('IDENTIFIER','IDENTIFIER')';
+     
+pop: 'POP''('IDENTIFIER','IDENTIFIER')';
